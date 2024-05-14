28c7063e6b37f08719b9713dae9a4dc935096461
Kellen <kellenweagle@gmail.com>
2024-05-13 21:45:44 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
+const { ValidationError } = require('sequelize');
+
 // Create a variable called isProduction that will be true if the environment is in production or not by checking the environment key in the configuration file (backend/config/index.js):
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -47,5 +49,40 @@ app.use(
 );
 
 app.use(routes);
+
+// regular middleware - will catch any req that don't match any routes and create a server error 404
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// catches sequelize errors and formats them before sending err response
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// formats errors before returning the JSON response
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
