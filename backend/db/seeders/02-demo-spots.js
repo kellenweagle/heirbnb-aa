b4e2e7cd336e81e772fb7b0fce6bfc09934bff2c
Kellen <kellenweagle@gmail.com>
2024-05-31 16:30:01 -0700

    fixing small bug

--- backend/db/seeders/02-demo-spots.js
+++ backend/db/seeders/02-demo-spots.js
@@ -94,7 +94,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Spot', null, {})
+    await queryInterface.bulkDelete(options, null, {})
   }
     
 }
