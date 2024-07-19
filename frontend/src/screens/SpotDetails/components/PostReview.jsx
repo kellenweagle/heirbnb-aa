0bef356fc56a5ff6748c36b31cf739958ed90887
Kellen <kellenweagle@gmail.com>
2024-07-19 13:30:15 -0700

    redepoly build

--- frontend/src/screens/SpotDetails/components/PostReview.jsx
+++ frontend/src/screens/SpotDetails/components/PostReview.jsx
@@ -1,49 +1,49 @@
-// frontend/src/components/LoginFormModal/LoginFormModal.jsx
-import { useDispatch } from 'react-redux';
-import { useModal } from '../../context/Modal';
-import './PostReview.css'
+// // frontend/src/components/LoginFormModal/LoginFormModal.jsx
+// // import { useDispatch } from 'react-redux';
+// // import { useModal } from '../../context/Modal';
+// import './PostReview.css'
 
-function PostReviewModal() {
-  const dispatch = useDispatch();
-  const { closeModal } = useModal();
+// function PostReviewModal() {
+//   // const dispatch = useDispatch();
+//   // const { closeModal } = useModal();
 
-  const [form, setForm] = useState({
-    description: "",
-  })
+//   // const [form, setForm] = useState({
+//   //   description: "",
+//   // })
 
-  const updateForm = (val, key) => {
-    return setForm((prev) => {
-      const newPrev = { ...prev };
-      newPrev[key] = val
-      return newPrev
-    })
-  }
+//   const updateForm = (val, key) => {
+//     return setForm((prev) => {
+//       const newPrev = { ...prev };
+//       newPrev[key] = val
+//       return newPrev
+//     })
+//   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+//   // const handleSubmit = async (e) => {
+//   //   e.preventDefault();
 
-    await dispatch()
+//   //   await dispatch()
 
-    closeModal()
-  };
+//   //   closeModal()
+//   // };
 
-  return (
-    <div
+//   return (
+//     <div
      
-     className='spot-review-form'
-     >
-      <h1>How was your stay?</h1>
-      <div className='describe-review'>
-          <textarea
-            className='description-textarea'
-            placeholder='Leave your review here...'
-            value={form.description}
-            onChange={(e) => updateForm(e.target.value, "description")}
-          />
-        </div>
-        <button className='post-review'>Submit your review</button>
-    </div>
-  );
-}
+//      className='spot-review-form'
+//      >
+//       <h1>How was your stay?</h1>
+//       <div className='describe-review'>
+//           <textarea
+//             className='description-textarea'
+//             placeholder='Leave your review here...'
+//             value={form.description}
+//             onChange={(e) => updateForm(e.target.value, "description")}
+//           />
+//         </div>
+//         <button className='post-review'>Submit your review</button>
+//     </div>
+//   );
+// }
 
-export default PostReviewModal;
+// export default PostReviewModal;
